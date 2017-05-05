51b0dfb6a803310210426dde09fafe25348901ca
Isao Abe <abe@nana-system.com>
2017-05-05 17:26:19 +0900

    First push

---
+++ Assets/Lesson1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lesson1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Person p = new Person ();
+		p.name = "test";
+		Debug.Log(p.name);
+		Debug.Log("aho");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+public class Person {
+	public string name;
+}
+
